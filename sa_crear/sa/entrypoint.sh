8133614d50f2204570ec620d56116a3017d14916
Javier Fernández Peón <javierfp@iessanclemente.net>
2020-05-17 17:34:52 +0200

    Montar sa en inicio

--- sa_crear/sa/entrypoint.sh
+++ sa_crear/sa/entrypoint.sh
@@ -3,6 +3,9 @@
 #Arrancamos servicio ssh
 service ssh start
 
+#Invocamos a mount para montar entradas en /etc/fstab
+mount -a
+
 #Uso exec para lanzar un proceso independiente de bucle infinito
 exec bash -c "while true;do sleep 10;done"
 
